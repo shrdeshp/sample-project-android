dc9f66f37f1be7be59b0b775230c00e19048e23c
Shrinivas Deshpande <shrdeshp@cisco.com>
2014-10-08 11:04:00 -0400

    Initial commit

---
+++ App/build/generated/source/buildConfig/defaultFlavor/release/com/example/app/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.app";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "defaultFlavor";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "";
+  /**
+   * @deprecated Use {@link #APPLICATION_ID}
+   */
+  @Deprecated
+  public static final String PACKAGE_NAME = "com.example.app";
+}
